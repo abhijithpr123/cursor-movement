24fe3f4cd9bcda3a4a1e72443a872e06b697d11d
abhijithpr123 <abhipr222@gmail.com>
2025-10-07 12:49:05 +0530

    cursor animation completed

---
+++ index.js
@@ -0,0 +1,7 @@
+let cursor = document.getElementById("cursor");
+document.addEventListener('mousemove',function(e){
+    cursor.style.left = e.clientX -15+ 'px';
+    cursor.style.top = e.clientY -15+ 'px';
+document.body.style.cursor = "url('/icons8-cursor.svg'), auto";
+
+});
